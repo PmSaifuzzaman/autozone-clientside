3adba814ba53a96c9d8f58c180866331da36d2d5
Saifuzzaman <pmsaifuzzaman@gmail.com>
2023-10-18 02:34:09 +0600

    Brand categoris data added to the homepage dinamically

---
+++ src/components/BrandCard/BrandCard.jsx
@@ -0,0 +1,26 @@
+import PropTypes from 'prop-types';
+
+const BrandCard = ({ brand }) => {
+
+    const { name, photo } = brand;
+
+    return (
+        <div className="card h-96 bg-base-100 shadow-xl">
+            <figure className="px-10 pt-10">
+                <img src={photo} alt="Shoes" className="rounded-xl h-full" />
+            </figure>
+            <div className="card-body items-center text-center">
+                <h2 className="card-title">{name}</h2>
+            </div>
+            <div className="card-actions justify-end pb-10 pr-10">
+                <button className="btn bg-sky-400 text-white">See All Product</button>
+            </div>
+        </div>
+    );
+};
+
+export default BrandCard;
+
+BrandCard.propTypes = {
+    brand: PropTypes.object,
+}
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,13 +1,22 @@
+import { useLoaderData } from "react-router-dom";
 import Banner from "../../components/Banner/Banner";
 import Navbar from "../../components/Navbar/Navbar";
+import BrandCard from "../../components/BrandCard/BrandCard";
 
 
 const Home = () => {
+    const brands = useLoaderData()
+    console.log(brands)
     return (
         <div>
             <Navbar></Navbar>
             <Banner></Banner>
-            <h2>This is home page</h2>
+            <h2 className="text-center text-4xl font-bold py-10">Our Popular Brands: {brands.length}</h2>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
+                {
+                    brands.map(brand => <BrandCard key={brand._id} brand={brand}></BrandCard>)
+                }
+            </div>
         </div>
     );
 };
